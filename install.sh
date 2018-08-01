845846bf97ea5de03f2deb8f71be48bad3588444
BialkowskiSz <bialkowski.sz@gmail.com>
2018-08-01 21:38:25 +0100

    Ensure vundle folder doesn't exist

--- install.sh
+++ install.sh
@@ -28,7 +28,9 @@ fi
 cp ./vim/.vimrc ~/.vimrc_new
 
 #   Vundle install
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 vim +PluginInstall +qall &
 
