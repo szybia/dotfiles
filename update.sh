e588357b6e6b5d3371befb137e8b7214652f47d1
BialkowskiSz <bialkowski.sz@gmail.com>
2018-08-06 14:18:41 +0100

    Update script

---
+++ update.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if ! hash git 2>/dev/null || ! hash curl 2>/dev/null; then
+    echo "git or curl is not installed"
+    echo "Please ensure both are installed and rerun the script"
+    exit 127
+fi
+
+if [ -f ~/.bashrc ] || [ -f ~/.vimrc  ]; then
+    echo ".bashrc or .vimrc has been found in your home folder."
+    echo "Your files will be renamed to <filename>_old"
+    read -p "Are you sure you want to proceed? (y/n): " yn
+    case $yn in 
+        [^Yy]) exit 0;;
+    esac
+    
+    #   Copy .bashrc and rerun
+    if [ -f ~/.bashrc ]; then
+        mv ~/.bashrc ~/.bashrc_old
+    fi
+
+    #   Ensure no .vimrc before Vundle install (themes will cause errors)
+    if [ -f ~/.vimrc ]; then
+        mv ~/.vimrc ~/.vimrc_old
+    fi
+fi
+
+cp ./bash/.bashrc ~/.bashrc
+source ~/.bashrc
+
+cp ./vim/.vimrc ~/.vimrc
+
+#   Ensure all .vim folders exist
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/swapfiles
+mkdir -p ~/.vim/undodir
+
+vim +PluginInstall +qall
+
