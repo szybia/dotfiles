b31ca4978bc5776b71cef79d4aba406da276377f
BialkowskiSz <bialkowski.sz@gmail.com>
2018-12-10 14:10:04 +0000

    remove yubitouch

--- test.sh
+++ test.sh
@@ -6,14 +6,14 @@ set -o pipefail
 
 ERRORS=()
 
-for f in $(find . -type f -not -iwholename '*.git*' -not -name "yubitouch.sh" | sort -u); do
-	if file "$f" | grep --quiet shell; then
-		{
-			shellcheck "$f" && echo "[OK]: successfully linted $f"
-		} || {
-			ERRORS+=("$f")
-		}
-	fi
+for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
+    if file "$f" | grep --quiet shell; then
+        {
+            shellcheck "$f" && echo "[OK]: successfully linted $f"
+        } || {
+            ERRORS+=("$f")
+        }
+    fi
 done
 
 if [ ${#ERRORS[@]} -eq 0 ]; then
