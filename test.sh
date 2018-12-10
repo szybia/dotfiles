771e007119b11e0fe0e63d8a0a19d3b108efcce3
BialkowskiSz <bialkowski.sz@gmail.com>
2018-12-10 14:09:01 +0000

    remove comments

--- test.sh
+++ test.sh
@@ -6,13 +6,11 @@ set -o pipefail
 
 ERRORS=()
 
-# find all executables and run `shellcheck`
 for f in $(find . -type f -not -iwholename '*.git*' -not -name "yubitouch.sh" | sort -u); do
 	if file "$f" | grep --quiet shell; then
 		{
 			shellcheck "$f" && echo "[OK]: successfully linted $f"
 		} || {
-			# add to errors
 			ERRORS+=("$f")
 		}
 	fi
