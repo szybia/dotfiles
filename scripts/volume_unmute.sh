3a90e3f4c0e7f5e0622bbf1fb0cee882fc9be26b
Szymon Bialkowski <bialkowski.sz@gmail.com>
2019-04-25 22:29:36 +0100

    Add script for unmuting amixer when it's bugged

---
+++ scripts/volume_unmute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# takes a volume level from 0 to 100, inclusive, as command line argument
+# every second, this program unmutes various channels and sets the Master volume to that volume, ensuring that the user hasn't turned the sound off or down
+
+while true; do
+  amixer set Master "$*%" unmute > /dev/null # $* gets volume from command line arg
+  amixer set Headphone unmute > /dev/null
+  amixer set Speaker unmute > /dev/null
+  amixer set PCM 100% unmute > /dev/null
+  break
+done
